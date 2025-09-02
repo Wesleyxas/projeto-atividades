a4f224483066c730beb18cc09d7fe6b0dc12d670
Wesley <whcjunim@gmail.com>
2025-09-02 15:03:54 -0300

    feat: Implementa lógica do botão para gerar citação aleatória

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const citacoes = [
   "A imaginação é mais importante que o conhecimento. - Albert Einstein",
   "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
-  "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill"
+  "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill",
 ];
 
 const citacaoEl = document.getElementById("citacao");
